9ef7b1e1cda373f5c6822b61c9a07501c83058a6
filibertolee <filibertolee@gmail.com>
2022-12-28 11:48:53 -0600

    Correcting println line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("_________********Hello World & Welcome to Jenkins Training**********________");
-		System.out.println("Adding second line*************);
+		System.out.println("Adding second line*************");
 	}
 }
 
